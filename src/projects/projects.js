229dd73f1996d18fd444d0a4ff58d468223740ce
Lana Radic <lanaradic1990@gmail.com>
2023-09-23 18:11:31 +0200

    pics update

--- src/projects/projects.js
+++ src/projects/projects.js
@@ -1,18 +1,18 @@
 export const data = [
 	{
 		title: 'My first game',
-		image: '/src/assets/spock.png',
+		image: '/spock.png',
 	},
 	{
 		title: 'Log in form',
-		image: '/src/assets/login.png',
+		image: '/login.png',
 	},
 	{
 		title: 'To do list',
-		image: '/src/assets/to-do-list-2.png',
+		image: '/to-do-list-2.png',
 	},
 	{
 		title: 'Weather app',
-		image: '/src/assets/weatherapp.png',
+		image: '/weatherapp.png',
 	},
 ];
