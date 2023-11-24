e48bcbc4d5105e1729c84ee719c9ec4bd82606bc
Lana Radic <lanaradic1990@gmail.com>
2023-11-24 08:50:51 +0100

    background game update

--- src/components/Gamee.jsx
+++ src/components/Gamee.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import '../components/styles/gamee.scss';
-import boltl from '../assets/bolt-up-left.png';
-import boltr from '../assets/bolt-up-right.png';
 
 const Gamee = () => {
 	const [computerSelection, setComputerSelection] = useState(null);
@@ -79,13 +77,15 @@ const Gamee = () => {
 	return (
 		<>
 			<div className="game">
-				<div className="game-box">
-					<div className="bolt-wrapper">
-						<img className="bolt-l" src={boltl} height="24px" alt="icon" />
-						<img className="bolt-r" src={boltr} height="24px" alt="icon" />
-					</div>
+				<div
+					style={{
+						backgroundImage:
+							"url('src/assets/depositphotos_32108825-stock-photo-blue-sunrise-view-of-earth.jpg')",
+					}}
+					className="game-box"
+				>
 					<div className="title-w">
-						<span>*Rock Paper Scissors Lizard Spock*</span>
+						<span>***MY GAME***</span>
 					</div>
 					<div className="score-wrapper">
 						<div className="show-score">SCORE: </div>
